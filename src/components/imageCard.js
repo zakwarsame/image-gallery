feba319cc3a8747a2b33276456ae67ace68528ea
zealchemy <zakatu2018@gmail.com>
2020-04-12 19:32:38 -0400

    add a loading functionality

--- src/components/imageCard.js
+++ src/components/imageCard.js
@@ -27,8 +27,8 @@ const imageCard = ({ image }) => {
         </ul>
       </div>
       <div className="px-6 py-4">
-        {tags.map(tag => (
-            <span key={image.id} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        {tags.map((tag, index) => (
+            <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
             #{tag}
           </span>
         ))}
