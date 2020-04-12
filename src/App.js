9f46927469de7e677cdb2eb871157d63125fa90c
zealchemy <zakatu2018@gmail.com>
2020-04-12 19:14:10 -0400

    connected the app with the api

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from 'react';
 
 
 function App() {
-  cosnt [images, setImages]= useState([]);
+  const [images, setImages]= useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
 
   useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}s&image_type=photo&pretty=true`)
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
     .then(res => res.json())
     .then(data => console.log(data))
     .catch(err => console.log(err));
 
-  });
+  }, []);
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
